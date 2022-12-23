e5ab5c2893306d06f5e0ecd21a1edc6cfb2ab0b8
vidhy <vidhy@LavanyaRVD>
2022-12-23 12:09:02 +0530

    my 1st commit

---
+++ LAVANYA99/src/main/java/LAVANYA99/LAVANYA99/App.java
@@ -0,0 +1,13 @@
+package LAVANYA99.LAVANYA99;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ LAVANYA99/src/main/java/sample1/test1.java
@@ -0,0 +1,5 @@
+package sample1;
+
+public class test1 {
+//hello welcome
+}
